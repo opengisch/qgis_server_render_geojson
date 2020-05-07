f64d05a45335edd25b5dc9d8adcc10619a8fd85b
Matthias Kuhn <matthias@opengis.ch>
2020-05-07 10:00:42 +0200

    Add warning for qgis desktop
    Fixes #3

--- qgis_server_render_geojson/__init__.py
+++ qgis_server_render_geojson/__init__.py
@@ -4,6 +4,26 @@
 """
 
 
+def classFactory(iface):
+    from qgis.PyQt.QtWidgets import QMessageBox
+
+    class Nothing:
+
+        def __init__(self, iface):
+            self.iface = iface
+
+        def initGui(self):
+            QMessageBox.warning(
+                self.iface.mainWindow(),
+                'qgis_server_render_geojson plugin',
+                'qgis_server_render_geojson is a plugin for QGIS Server. There is nothing in QGIS Desktop.',
+            )
+
+        def unload(self):
+            pass
+
+    return Nothing(iface)
+
 def serverClassFactory(serverIface):
     from .render_geojson import RenderGeojsonServer
     return RenderGeojsonServer(serverIface)
